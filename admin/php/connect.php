84d35fd10762c1b84e002d947346728038769050
chanyo19 <chandimal.zone@gmail.com>
2019-09-09 15:24:44 +0530

    changed the database credentials for the web

--- admin/php/connect.php
+++ admin/php/connect.php
@@ -12,10 +12,10 @@ if ($local_conn) {
 } 
 else {
     //intialize the connections for web
-    $db_host = "localhost";
-    $db_username = "";
-    $db_password = "";
-    $db_name = "";
+    $db_host = "sql306.epizy.com";
+    $db_username = "epiz_24449884";
+    $db_password = "VNhLTI4p85";
+    $db_name = "epiz_24449884_chandimal";
 	//echo "connected";
 	
 }
